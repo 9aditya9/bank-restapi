9b2d3ee441afea71f2818ce93b14732461b2c4a3
9aditya9 <75698641+9aditya9@users.noreply.github.com>
2021-05-29 20:12:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+# import os
+# from flask import Flask, request, jsonify
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import scoped_session, sessionmaker
+# # from flask_sqlalchemy import SQLAlchemy
+
+# app = Flask(__name__)
+
+# engine = create_engine(os.getenv("DATABASE_URI"))
+# db = scoped_session(sessionmaker(bind=engine))
+
+
+# @app.route("/", methods=["GET"])
+# def autocomplete():
+# 	q = request.args.get('q')
+# 	if not q:
+# 		q = 'STATE'
+# 	result = db.execute("SELECT bank_name FROM bank_branches WHERE (bank_name LIKE :q) LIMIT 3", {"q": q + "%"})
+# 	# return jsonify({"result": [dict[row] for row in result]})
+# 	return jsonify({'result': [dict(row) for row in result]})
+
+
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+db = SQLAlchemy(app)
+
+
+@app.route("/", methods=["GET"])
+def auto():
+	q = 'STATE'
+	result = db.session.execute("SELECT bank_name FROM bank_branches WHERE (bank_name LIKE :q) LIMIT 3", {"q": q + "%"}).fetchall()
+	return jsonify({"result": [dict(row) for row in result]})
