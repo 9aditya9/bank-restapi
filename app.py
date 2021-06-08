9e5cf5a2f6c6e3a1e36abf45776dc6a07153b84c
9aditya9 <75698641+9aditya9@users.noreply.github.com>
2021-06-08 13:18:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -23,10 +23,9 @@
 import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
 db = SQLAlchemy(app)
 
@@ -35,4 +34,32 @@ db = SQLAlchemy(app)
 def auto():
 	q = 'STATE'
 	result = db.session.execute("SELECT bank_name FROM bank_branches WHERE (bank_name LIKE :q) LIMIT 3", {"q": q + "%"}).fetchall()
+	return jsonify({"result": [dict(row) for row in result]})
+
+@app.route("/api/branches/autocomplete")
+def autocomplete():
+	q = request.args.get('q')
+	limit = request.args.get('limit')
+	offset = request.args.get('offset')
+	if not offset:
+		offset = 0
+	if not limit:
+		limit = 5
+	if not q:
+		q = 'A'
+	result = db.session.execute("SELECT * FROM bank_branches WHERE (branch LIKE :q) ORDER BY ifsc ASC LIMIT (:limit) OFFSET (:offset)", {"q": q + "%", "limit": int(limit), "offset": int(offset)}).fetchall()
+	return jsonify({"result": [dict(row) for row in result]})
+
+@app.route("/api/branches")
+def searchapi():
+	q = request.args.get('q')
+	limit = request.args.get('limit')
+	offset = request.args.get('offset')
+	if not offset:
+		offset = 0
+	if not limit:
+		limit = 5
+	if not q:
+		q = 'A'
+	result = db.session.execute("SELECT * FROM bank_branches WHERE (branch LIKE :q) OR (address LIKE :q) OR (city LIKE :q) OR (district LIKE :q) OR (state LIKE :q) ORDER BY ifsc ASC LIMIT (:limit) OFFSET (:offset)", {"q": q + "%", "limit": int(limit), "offset": int(offset)}).fetchall()
 	return jsonify({"result": [dict(row) for row in result]})
